77569c9c0d5ecbcc0c9c809740f30ec1d9b3129f
Rami214 <>
2021-11-16 11:03:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,106 @@
+# # This computer can only add and subtract
+# Jane = input("Please enter your name: ")
+# num1 = 9
+# num2 = 5
+
+# print("Do youw ant me to (a)dd or (s)ubtract?")
+# choice = input()
+
+# if choice == "a" :
+#    print(num1 + num2)
+# elif choice == "s":
+#  print(num1 - num2)
+
+# a = input ("Enter the first number: ")
+# if a ("Enter the second number: ")
+
+
+#EXAM
+
+#### Describe each datatype below:(4 marks)
+
+## 4         = interger
+## 5.7       = float
+## True      = boolean
+## Good Luck = string
+
+#### Which datatype would be useful for storing someone's height? (1 mark)
+## Float
+
+#### Which datatype would be useful for storing someone's hair colour?(1 mark)
+##  String
+
+
+####Create a variable tha will store the users name.(1 mark)
+# name = input ("Enter your username if you want to store it: ")
+# print (name)
+
+####Create a print statement that will print the first 4 characters of the person's name.(3 marks)
+# name = input ("Enter your name to see the first 4 letters of your name: ")
+# print (name[0:4])
+
+####Convert the user's name to all uppercase characters and print the result
+# name = input("What is your name? It will be printed in uppercase letters : ")
+# print (name.upper())
+
+
+####Find out how many times the letter A occurs in the user's name 
+# count = input("Enter your name to find out the A count in your name: ")
+# print (count.count("a"))
+
+
+#### Create a conditional statement to ask a user to enter their age. If they are older than 18 they receive a message saying they can enter the competition, if they are under 18, they receive a message saying they cannot enter the competition.
+# age = int(input("Enter your age: "))
+# if age >18 :
+#  print("You can enter the competition")
+# elif age <18 :
+#   print("Your are not eligable to enter the competition")
+
+
+#### Create an empty list called squareNumbers (3 marks)
+# squareNumbers = []
+#### Square numbers are the solutions to a number being multiplied by itself( example 1 is a square number because 1 X 1 = 1, 4 is a square number because 2 X 2 = 4 ). 
+###Calculate the first 20 square numbers and put them in the list called squareNumbers. (With loop and .append 10 marks, without, Max 6 marks).
+# squareNumbers = [2, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400]
+
+####Print your list (1 mark)
+# print(squareNumbers)
+####Create a variable called userSquare that asks the user for their favourite square number
+# userSquare = input("Enter your favourite squared number: ")
+# print(userSquare)
+
+
+#### Add this variable to the end of your list called SquareNumbers
+# userSquare = input("Enter your favourite squared number: ")
+# squareNumbers = [2, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400, (userSquare)]
+# print(squareNumbers)
+### Create a variable called choice. This variable should choose a random element from your list. Print the variable called choice.(3 marks)
+
+####Create another print statement that prints tha following output: The random square number is: XX, where XX is where the random square number chosen by the computer.(4 marks)
+# import random
+# choice = int(random.choice(squareNumber))
+# print(choice)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
